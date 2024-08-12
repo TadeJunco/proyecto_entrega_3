91c717297dd5f05ad69e123a9fd86825287b0c63
Tadeo Junco <tade.junco@gmail.com>
2024-08-12 19:55:28 -0300

    tartando de avanzar lpm

--- AppTade/urls.py
+++ AppTade/urls.py
@@ -1,18 +1,18 @@
-from AppTade import views
 from django.urls import path
+from AppTade import views
+
 
 urlpatterns = [
-    path('', views.inicio, name="inicio"),
-    path('profesor/',views.profesor, name="profesor"),
-    path('cursos',views.cursos, name="cursos"),
-    path('entregables/', views.entregables, name="Entregables"),
+    path('', views.inicio, name="Inicio"),
+    path('profesor/', views.profesores, name="Profesor"),
     path('estudiantes/', views.estudiantes, name="Estudiantes"),
-]
-
-clase_21 = [
-    path('form-comun/', views.curso_formulario, name="Form-Comun"),
+    path('cursos/', views.cursos, name="Cursos"),
+    path('entregables/', views.entregables, name="Entregables"),
+    path('form-comun/', views.form_comun, name="Form-Comun"),
     path('form-con-api/', views.form_con_api, name="Form-Con-Api"),
-    path('buscar-form-con-api/', views.buscar_form_con_api, name="Buscar_Form_Con_Api"),
+    path('buscar-form-con-api/', views.buscar_form_con_api, name="Buscar-Form-Con-Api"),
 ]
 
-urlpatterns += clase_21
+
+
+
--- AppTade/views.py
+++ AppTade/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from .models import Curso
 from AppTade.forms import CursoFormulario, BuscaCursoForm
 
-
 def inicio(request):
     return render(request, "App1/index.html")
 
-def profesor(request):
-    return render(request, "App1/profesor.html")
-
 def cursos(request):
     return render(request, "App1/cursos.html")
+
+def profesores(request):
+    return render(request, "App1/profesores.html")
 
 def estudiantes(request):
     return render(request, "App1/estudiantes.html")
@@ -18,47 +17,44 @@ def estudiantes(request):
 def entregables(request):
     return render(request, "App1/entregables.html")
 
-
-def curso_formulario(request):
+def form_comun(request):
 
     if request.method == 'POST':
 
-        curso = Curso(nombre=request.POST['curso'],camada=request.POST['camada'])
+        curso =  Curso(nombre=request.POST['curso'],camada=request.POST['camada'])
         curso.save()
 
         return render(request, "App1/index.html")
 
-    return render(request,"App1/curso_formulario.html")
+    return render(request,"App1/form_comun.html")
 
 def form_con_api(request):
     if request.method == "POST":
-        mi_formulario = CursoFormulario(request.POST) 
-        if mi_formulario.is_valid():
-            informacion = mi_formulario.cleaned_data
-            
-            curso = Curso(nombre=informacion["nombre"], camada=informacion["camada"])
+        miFormulario = CursoFormulario(request.POST) # Aqui me llega la informacion del html
+        # print(miFormulario)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            curso = Curso(nombre=informacion["curso"], camada=informacion["camada"])
             curso.save()
-
             return render(request, "App1/index.html")
     else:
-        mi_formulario = CursoFormulario()
+        miFormulario = CursoFormulario()
 
-    return render(request, "App1/form_con_api.html", {"mi_formulario": mi_formulario})
-
+    return render(request, "App1/form_con_api.html", {"miFormulario": miFormulario})
 
 def buscar_form_con_api(request):
     if request.method == "POST":
-        mi_formulario = BuscaCursoForm(request.POST) 
+        miFormulario = BuscaCursoForm(request.POST) # Aqui me llega la informacion del html
 
-        if mi_formulario.is_valid():
-            informacion = mi_formulario.cleaned_data
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
             
             cursos = Curso.objects.filter(nombre__icontains=informacion["curso"])
 
-            return render(request, "App1/mostrar_cursos.html", {"cursos": cursos})
+            return render(request, "App1/resultados_buscar_form.html", {"cursos": cursos})
     else:
-        mi_formulario = BuscaCursoForm()
+        miFormulario = BuscaCursoForm()
 
-    return render(request, "App1/buscar_form_con_api.html", {"mi_formulario": mi_formulario})
+    return render(request, "App1/buscar_form_con_api.html", {"miFormulario": miFormulario})
 
 
